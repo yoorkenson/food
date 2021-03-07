6ba5f484c5edcb40a58478ac6cc864807d80e00d
Yoora <yoorkenson@gmail.com>
2021-03-07 16:25:20 +0400

    post_server_script

--- js/main.js
+++ js/main.js
@@ -149,7 +149,7 @@ window.addEventListener('DOMContentLoaded', () => {
     // Используем классы для карточек
 
     class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector) {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.title = title;
@@ -158,6 +158,7 @@ window.addEventListener('DOMContentLoaded', () => {
             this.transfer = 27;
             this.changeToUAH();
             this.parent = document.querySelector(parentSelector);
+            this.classes = classes;
         }
 
         changeToUAH() {
@@ -166,16 +167,21 @@ window.addEventListener('DOMContentLoaded', () => {
 
         render() {
             const element = document.createElement('div');
+            if (this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
             element.innerHTML = `
-                <div class="menu__item">
-                    <img src=${this.src} alt=${this.alt}>
-                    <h3 class="menu__item-subtitle">${this.title}</h3>
-                    <div class="menu__item-descr">${this.descr}</div>
-                    <div class="menu__item-divider"></div>
-                    <div class="menu__item-price">
-                        <div class="menu__item-cost">Цена:</div>
-                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                    </div>
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                 </div>
             `;
             this.parent.append(element);
@@ -188,7 +194,8 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню "Фитнес"',
         'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
         9,
-        '.menu .container'
+        '.menu .container',
+
     ).render(); 
 
     new MenuCard(
@@ -197,7 +204,8 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню “Премиум”',
         'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - настоящее ресторанное меню, при этом без похода в ресторан!',
         14,
-        '.menu .container'
+        '.menu .container',
+        'menu__item'
     ).render(); 
 
     new MenuCard(
@@ -206,6 +214,62 @@ window.addEventListener('DOMContentLoaded', () => {
         'Меню "Постное"',
         'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
         21,
-        '.menu .container'
+        '.menu .container',
+        'menu__item'
     ).render(); 
+
+    // FORMS
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'загрузка',
+        success: 'спасибо',
+        failure: 'не получилось'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) { //ф-я постинга данных
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            
+            // request.setRequestHeader('Content-type', 'nultipart/form-data'); //для form-data не надо писать заголовок
+            request.setRequestHeader('Content-type', 'application/json'); // для json
+            const formData = new FormData(form);
+            // request.send(formData); //для form data
+            const object ={};
+            formData.forEach(function(value, key) {
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+
+            request.send(json);
+
+
+            request.addEventListener('load', () => {
+               if (request.status === 200) {
+                   console.log(request.response);
+                   statusMessage.textContent = message.success;
+                   form.reset();
+                   setTimeout(() => {
+                        statusMessage.remove();
+                   }, 2000);
+               } else {
+                statusMessage.textContent = message.failure;
+               }
+            });
+        });
+    }
 });
